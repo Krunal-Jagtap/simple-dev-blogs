ec64a39733892ad8e6c2c70d9437906ad438eaa7
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-07-15 23:06:25 +0530

    Feedback - Animation is slow | Fixed ✅

--- Animation_Script.js
+++ Animation_Script.js
@@ -3,27 +3,27 @@ var tl = gsap.timeline();
 tl.from("header", {
   y: -30,
   opacity: 0,
-  duration: 1,
+  duration: 0.5,
   delay: 0.2,
 });
 
 tl.from("main", {
   y: 30,
   opacity: 0,
-  duration: 0.5,
+  duration: 0.3,
   scale: 0.2,
 });
 
 tl.from(".blog-titles a", {
   y: -30,
   opacity: 0,
-  duration: 0.5,
-  stagger: 0.5,
+  duration: 0.3,
+  stagger: 0.2,
 });
 
 tl.from("footer", {
   y: 30,
   opacity: 0,
-  duration: 0.5,
+  duration: 0.2,
   scale: 0.2,
 });
